c2c14b99c1ea62ee921ebdfcc398ab8df495f508
mhmtztmr <mahmutoztemur@gmail.com>
2017-12-10 22:33:54 +0300

    Add content/title separation for font size change

--- demo/src/components/app.js
+++ demo/src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import { FontSizeChanger } from '../../../dist/bundle';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div>Change the whole text</ div>
         <FontSizeChanger
           targets={['#target']}
           options={{
@@ -13,9 +13,18 @@ class App extends Component {
             range: 3
           }}
         />
-        <div id="target" style={{'fontSize': '3em'}}>
-          This is my target text
+        <div id="target">
+          <p style={{'fontSize': '20px', 'fontWeight': 'bold' }}>This is the title of my target text</p>
+          <p className="content" style={{'fontSize': '14px'}}>This is the content of my target text</p>
         </div>
+        <div>Change content only</ div>
+        <FontSizeChanger
+          targets={['#target .content']}
+          options={{
+            stepSize: 2,
+            range: 3
+          }}
+        />
       </div>
     );
   }
